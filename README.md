26fda43a7360eb96c6debb9e9f695c0d6f1e3503
Martina Volfova <vlcice.martina@gmail.com>
2018-09-11 22:35:16 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# PigLatin translator game
---
+++ src/Translator/Translator.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Vlcice\PigLatin\Translator;
+
+
+/**
+ * Class Translator
+ * @package Vlcice\PigLatin;
+ */
+class Translator
+{
+}
