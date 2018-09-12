d0135c19fcc83f7c5b1e2064f1a2f12ad5b92145
Martina Volfova <vlcice.martina@gmail.com>
2018-09-12 19:16:53 +0200

    zmeny

--- src/Translator/Translator.php
+++ src/Translator/Translator.php
@@ -9,4 +9,115 @@ namespace Vlcice\PigLatin\Translator;
  */
 class Translator
 {
+    const CONSONANTS = 'bcdfghjklmnpqrstuvwxz'; //y
+    const VOWELS = 'aeiou'; //y
+    const SEMIVOWEL = 'y';
+
+    /**
+     * A sillable added to the end of the word.
+     */
+    const SUFFIX_SYLLABLE = 'ay';
+
+    /**
+     * @var array
+     */
+    private $silent_consonants = [
+        'mb', 'tle', 'gh', 'kn', 'wr', 'ght'
+    ];
+
+    /**
+     * Input string.
+     * @var string
+     */
+    private $input = '';
+
+    /**
+     * Output string.
+     * @var string
+     */
+    private $output = '';
+
+    /**
+     * Error message if any.
+     * @var string
+     */
+    private $errorMessage = '';
+
+    /**
+     * @param string $letter
+     * @return bool
+     */
+    public function isConsonant(string $letter) : bool
+    {
+        return (strpos(self::CONSONANTS, $letter) !== false);
+    }
+
+    /**
+     * @param string $letter
+     * @return bool
+     */
+    public function isVowel(string $letter) : bool
+    {
+        return (strpos(self::VOWELS, $letter) !== false);
+    }
+
+    /**
+     * @param string $letter
+     * @return bool
+     */
+    public function isSemiVowel(string $letter) : bool
+    {
+        return $letter === self::SEMIVOWEL;
+    }
+
+    public function translate() : string
+    {
+        $matches = [];
+
+        if ($this->isValid()) {
+            $initialLetter = $this->input[0];   //faster than strpos
+
+            preg_match('/(^['.self::CONSONANTS.']*)(.*)$/', $this->input, $matches, PREG_OFFSET_CAPTURE);
+        }
+
+        return json_encode($matches);
+    }
+
+    /**
+     * Returns true if input string is valid.
+     * @return bool
+     */
+    public function isValid() : bool
+    {
+        $this->errorMessage = '';
+
+        if ($this->input === '') {
+            $this->errorMessage = "Empty string";
+        } elseif(!preg_match("/[a-z -\.]/i", $this->input)) {
+            $this->errorMessage = "The input contains a non-alphabet character.";
+        }
+
+        return ($this->errorMessage !== '');
+    }
+
+    /**
+     * Input setter.
+     * @param $input
+     * @return Translator
+     */
+    public function setInput($input) : self
+    {
+        $this->input = $input;
+
+        return $this;
+    }
+
+    /**
+     * ErrorMessage getter.
+     * @return string
+     */
+    public function getErrorMessage() : string
+    {
+        return $this->errorMessage;
+    }
 }
